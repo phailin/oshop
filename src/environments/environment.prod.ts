974c8926d1ef4b4fd5e19b6bd28e0a0f9198ff77
Netrananda Matari <nmatari3@in.ibm.com>
2018-05-24 11:05:23 +0530

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAE0X6azUPgO4h8dmynQBo01QA_TV9LDPY",
+    authDomain: "oshop-2fcbe.firebaseapp.com",
+    databaseURL: "https://oshop-2fcbe.firebaseio.com",
+    projectId: "oshop-2fcbe",
+    storageBucket: "oshop-2fcbe.appspot.com",
+    messagingSenderId: "2152299690"
+  },
+};
